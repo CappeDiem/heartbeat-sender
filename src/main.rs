b21d156f95a7d6db3c49b6783e72b100497bc8c2
CappeDiem <42583065+CappeDiem@users.noreply.github.com>
2024-03-20 12:34:17 +0200

    Initial implementation that works

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::{env, thread};
+use dotenv::dotenv;
+use std::error::Error;
+use std::time::{Duration, Instant};
+
 fn main() {
-    println!("Hello");
+    dotenv().expect("Failed to load .env file");
+    let scheduler = thread::spawn(|| {
+        loop {
+            let start = Instant::now();
+
+            let heartbeat_url = env::var("HEARTBEAT_URL").expect("Expected a token in the environment");
+            let interval = env::var("HEARTBEAT_INTERVAL").expect("Expected a token in the environment");
+
+            send_heartbeat(heartbeat_url).expect("TODO: panic message");
+            thread::sleep(Duration::from_millis(interval.parse::<u64>().unwrap()));
+            let duration = start.elapsed();
+            println!("interval: {:?}", duration);
+        }
+    });
+    scheduler.join().expect("Scheduler panicked");
+}
+
+fn send_heartbeat(url:String) -> Result<(), Box<dyn Error>> {
+    let resp = reqwest::blocking::get(url)?.text()?;
+    //println!("{:#?}", resp);
+    println!("INFO: Heartbeat Sent");
+    Ok(())
 }
